48981ecd91beae4f37b20b5d747436962c255dde
Jhumberto <jhumbertto@gmail.com>
2022-12-05 21:29:57 -0600

    Preguntas-funcionales

--- src/Components/Questions.jsx
+++ src/Components/Questions.jsx
@@ -7,23 +7,31 @@ function Questions (){
   const [preguntaActual, setPreguntaActual] = useState(0);
   const [puntuacion, setPuntuacion] = useState(0);
   const [isFinish, setIsFinished] = useState(false);
-  const [checkboxValue, setcheckboxValue] = useState(false)
+  const [checkBoxValue, setcheckboxValue] = useState(false)
 
   function showPrevQuestion(value, e){
       //regresar a pregunta anterior
         setPreguntaActual(preguntaActual - 1)
   }
-    function showNextQuestion(value, e){
+  function showNextQuestion(value, e){
       //cambiar a  la siguiente pregunta
         setPreguntaActual(preguntaActual + 1)
       
   }
+  function changeCheckBoxValue(value, e){
+    //cambiar el  valor true/false
+    setcheckboxValue(checkBoxValue)    
+}
   return (
     <div className='questionCard'>
       <h1 className='questions'>{preguntas[preguntaActual].titulo}</h1>
       <div className='answer'>
         {preguntas[preguntaActual].opciones.map((respuesta) =>(
-          <><input type="checkbox"/><label>{respuesta.textoRespuesta}</label></>
+          <><input 
+          type="checkbox" 
+          value={checkBoxValue}
+          onChange={(e)=>changeCheckBoxValue(e)}
+          /><label>{respuesta.textoRespuesta}</label></>
         ))}
       </div>
       <button
